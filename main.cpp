3b5455fca81404cbe12d39ca08426f3e004fa7ce
Chris Maughan <cmaughan@snarlsoftware.com>
2016-09-07 22:13:36 +0100

    Add GLM

--- main.cpp
+++ main.cpp
@@ -1,113 +1,146 @@
 #include <cstdio>
 #include <chrono>
-
+#include <algorithm>
 #include "writebitmap.h"
 #include "mathtypes.h"
 #include <vector>
 
 const int ImageWidth = 512;
 const int ImageHeight = 512;
-
+#define MAX_DEPTH 1
 std::vector<Sphere> spheres;
 
-Vector3 CameraPosition = Vector3{ 0.0f, -3.0f, 0.0f };
-Vector3 CameraLookDirection = Vector3{ 0.0f, 0.0f, 1.0f };
+vec3 CameraPosition = vec3{ 0.0f, -3.0f, 0.0f };
+vec3 CameraLookDirection = vec3{ 0.0f, 0.0f, 1.0f };
 float FieldOfView = 30.0f;
 
 void InitScene()
 {
-	spheres.push_back(Sphere{ Vector3{ -1.0f, 0.0f, -5.f }, 0.7f, Color3{1.0f, 0.0f, 0.0f} });
-	spheres.push_back(Sphere{ Vector3{ 1.0f, 0.0f, -5.f }, 0.5f, Color3{1.0f, 0.0f, 1.0f}  });
-	spheres.push_back(Sphere{ Vector3{ 0.0f, 0.0f, -7.f }, 0.5f, Color3{0.0f, 1.0f, 0.0f} });
+    spheres.push_back(Sphere{ vec3{ -1.0f, 0.0f, -7.5f }, 1.0f, vec3{0.5f, 0.0f, 0.0f},0.3f, vec3{.9f, .1f, .1f} });
+    spheres.push_back(Sphere{ vec3{ 1.0f, 0.0f, -9.f }, 1.0f, vec3{0.5f, 0.0f, 0.5f}, 1.f, vec3{.9f, .1f, .8f} });
+    spheres.push_back(Sphere{ vec3{ 0.0f, 1.2f, -7.5f }, 0.5f, vec3{0.0f, 0.5f, 0.0f}, .7f, vec3{.1f, .9f, .0f } });
+    //spheres.push_back(Sphere{ vec3{ 1.0f, .5f, -2.5f }, 0.5f, vec3{0.5f, 0.5f, 0.0f}, .5f });
 }
 
-Color3 TraceRay(const Vector3& rayorig, const Vector3 &raydir, const int &depth)
+vec3 TraceRay(const vec3& rayorig, const vec3 &raydir, const int &depth, bool& hit)
 {
-	float tnear = INFINITY;
-	const Sphere* sphere = NULL;
-	 
-	// find intersection of this ray with the sphere in the scene
-	for (unsigned i = 0; i < spheres.size(); ++i)
-	{
-		float t0 = INFINITY, t1 = INFINITY;
-		if (spheres[i].Intersect(rayorig, raydir, t0, t1)) 
-		{
-			if (t0 < 0)
-			{
-				t0 = t1;
-			}
+    const Sphere* sphere = NULL;
+    float lastDistanceSquared = INFINITY;
+    vec3 pos, normal;
 
-			if (t0 < tnear) 
-			{
-				tnear = t0;
-				sphere = &spheres[i];
-			}
-		}
-	}
+    // find intersection of this ray with the sphere in the scene
+    for (unsigned i = 0; i < spheres.size(); ++i)
+    {
+        vec3 intersectPos;
+        vec3 intersectNormal;
+        if (intersectRaySphere(rayorig, normalize(raydir), spheres[i].center, spheres[i].radius, intersectPos, intersectNormal))
+        {
+            auto d = fabs(distance(intersectPos, rayorig));
+            if (d < lastDistanceSquared)
+            {
+                lastDistanceSquared = d;
+                sphere = &spheres[i];
+                pos = intersectPos;
+                normal = normalize(intersectNormal);
+            }
+        }
+    }
 
-	if (!sphere)
-	{
-		return Color3{ 0.0f, 0.0f, 0.0f };
-	}
+    if (!sphere)
+    {
+        hit = false;
+        return vec3{ 0.0f, 0.0f, 0.0f };
+    }
 
-	// Where we hit
-	Vector3 phit = rayorig + raydir * tnear; 
+    hit = true;
+    vec3 reflect = normalize(glm::reflect(raydir, normal));
+    vec3 result{ 0.0f, 0.0f, 0.0f };
 
-	// Normal
-    Vector3 normal = phit - sphere->center; 
-    normal = -normal.Normalize(); 
+    float tiny = std::numeric_limits<float>::min();
 
-	if (sphere)
-	{
-		return sphere->color * normal.Dot(raydir);
-	}
+    vec3 outputColor{ 0.0f, 0.0f, 0.0f };
 
-	return Color3{ 0.0f, 0.0f, 0.0f };
+    // If the object is transparent, return a reflection color
+    if (depth < MAX_DEPTH && sphere->reflection > 0.0f)
+    {
+        outputColor = TraceRay(pos, reflect, depth + 1, hit) * sphere->reflection;
+    }
+
+    // Otherwise, sample the lighting
+    vec3 lightDir = vec3{ 1.f, .0f, 2.0f };
+    lightDir = normalize(lightDir);
+
+    vec3 specColor{ 1.0f, 1.0f, 1.0f };
+    vec3 diffuseColor{ 0.0f, 0.0f, 0.0f };
+
+    float diffuseI = dot(normal, lightDir);
+    float specI = 0.0f;
+    
+    if (diffuseI > 0.0f)
+    {
+        specI = dot(reflect, lightDir);
+        if (specI > 0.0f)
+        {
+            specI = pow(specI, 10);
+            specI = std::max(0.0f, specI);
+        }
+        else
+        {
+            specI = 0.0f;
+        }
+    }
+    else
+    {
+        diffuseI = 0.0f;
+    }
+    outputColor = outputColor + ( ((sphere->color * diffuseI) + (sphere->specularColor * specI)) /* (1.0f - sphere->transparency)*/) ;
+    return outputColor;
 }
 
 void DrawScene(Bitmap* pBitmap)
 {
-	float invWidth = 1.0f / float(ImageWidth);
-	float invHeight = 1.0f / float(ImageHeight);
+    float invWidth = 1.0f / float(ImageWidth);
+    float invHeight = 1.0f / float(ImageHeight);
 
-	float aspectRatio = float(ImageWidth) / float(ImageHeight);
+    float aspectRatio = float(ImageWidth) / float(ImageHeight);
 
-	float halfAngle = tan(DegreesToRadians(FieldOfView) * 0.5f);
+    float halfAngle = tan(glm::radians(FieldOfView) * 0.5f);
 
-	Color col{ 255, 0, 255 };
-	for (int y = 0; y < ImageHeight; y++)
-	{
-		for (int x = 0; x < ImageWidth; x++)
-		{
-			float xx = (2.0f * (((float)x + 0.5f) * invWidth) - 1.f) * halfAngle * aspectRatio;
-			float yy = (1.0f - 2.0f * (((float)y + 0.5f) * invHeight)) * halfAngle;
+    Color col{ 255, 0, 255 };
+    for (int y = 0; y < ImageHeight; y++)
+    {
+        for (int x = 0; x < ImageWidth; x++)
+        {
+            float xx = (2.0f * (((float)x + 0.5f) * invWidth) - 1.f) * halfAngle * aspectRatio;
+            float yy = (1.0f - 2.0f * (((float)y + 0.5f) * invHeight)) * halfAngle;
 
-			Vector3 rayDir{ xx, yy, -1.0f };
-			rayDir = rayDir.Normalize();
+            vec3 rayDir{ xx, yy, -1.0f };
+            rayDir = normalize(rayDir);
 
-			Color3 color = TraceRay(Vector3{ 0.0f, 0.0f, 0.0f }, rayDir, 0);
-			color = color * 255.0f;
-			color = color.Clamp(255.0f);
-			
-			PutPixel(pBitmap, x, y, Color{ uint8_t(color.x), uint8_t(color.y ),uint8_t(color.z) });
-		}
-	}
+            bool hit = false;
+            vec3 color = TraceRay(vec3{ 0.0f, 0.0f, 0.0f }, rayDir, 0, hit);
+            color = color * 255.0f;
+            color = clamp(color, vec3(0.0f, 0.0f, 0.0f), vec3(255.0f, 255.0f, 255.0f));
+
+            PutPixel(pBitmap, x, y, Color{ uint8_t(color.x), uint8_t(color.y),uint8_t(color.z) });
+        }
+    }
 }
 
 void main(void* arg, void** args)
 {
-	Bitmap* pBitmap = CreateBitmap(ImageWidth, ImageHeight);
+    Bitmap* pBitmap = CreateBitmap(ImageWidth, ImageHeight);
 
-	Color col{ 127, 127, 127 };
-	ClearBitmap(pBitmap, col);
+    Color col{ 127, 127, 127 };
+    ClearBitmap(pBitmap, col);
 
-	InitScene();
+    InitScene();
 
-	DrawScene(pBitmap);
+    DrawScene(pBitmap);
 
-	WriteBitmap(pBitmap, "image.bmp");
+    WriteBitmap(pBitmap, "image.bmp");
 
-	DestroyBitmap(pBitmap);
+    DestroyBitmap(pBitmap);
 
-	system("start image.bmp");
+    system("start image.bmp");
 }
--- mathtypes.h
+++ mathtypes.h
@@ -1,89 +1,16 @@
 #pragma once
 #include <cmath>
-static float DegreesToRadians(float degrees)
-{
-	return float((M_PI * degrees) / 180.0f);
-}
+#include "glm/glm/glm.hpp"
+#include "glm/glm/gtx/intersect.hpp"
 
-struct Vector3
-{
-	float x;
-	float y;
-	float z;
-
-	Vector3 operator-() const
-	{
-		return Vector3{ -x, -y, -z };
-	}
-	Vector3 operator- (const Vector3& rhs) const
-	{
-		return Vector3{ x - rhs.x, y - rhs.y, z - rhs.z };
-	}
-
-	Vector3 operator+ (const Vector3& rhs) const
-	{
-		return Vector3{ x + rhs.x, y + rhs.y, z + rhs.z };
-	}
-
-	Vector3 operator* (const float& rhs) const
-	{
-		return Vector3{ x * rhs, y * rhs, z * rhs };
-	}
-
-	Vector3 Clamp(float max)
-	{
-		return Vector3{ x <= 255.0f ? x : 255.0f,
-						y <= 255.0f ? y : 255.0f,
-						z <= 255.0f ? z : 255.0f };
-	}
-	Vector3 Normalize()
-	{
-		float length = sqrtf((x * x) + (y * y) + (z * z));
-		if (length > 0)
-		{
-			return Vector3{ x / length, y / length, z / length };
-		}
-		return *this;
-	}
-
-	float Dot(const Vector3& rhs)
-	{
-		return (x * rhs.x) + (y * rhs.y) + (z * rhs.z);
-	}
-};
-
-using Color3 = Vector3;
-using Ray = Vector3;
+using namespace glm;
 
 struct Sphere
 {
-	Vector3 center;
-	float radius;
-	Color3 color;
+    vec3 center;
+    float radius;
+    vec3 color;
+    float reflection;
+    vec3 specularColor;
 
-	bool Intersect(const Vector3& rayorig, const Vector3& raydir, float& t0, float& t1) const 
-    { 
-        Vector3 l = center - rayorig; 
-
-        float tca = l.Dot(raydir); 
-
-		if (tca < 0.0f)
-		{
-			return false;
-		}
-
-        float d2 = l.Dot(l) - tca * tca; 
-		 
-		if (d2 > (radius * radius))
-		{
-			return false;
-		}
-		
-        float thc = sqrtf((radius * radius) - d2); 
-
-        t0 = tca - thc; 
-        t1 = tca + thc; 
- 
-        return true; 
-    } 
 };
