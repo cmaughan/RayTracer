c83618db880992cb4cb438eaef050d7ee7e2eb1d
Chris Maughan <cmaughan@snarlsoftware.com>
2017-01-05 10:22:44 +0000

    Fixed camera

--- raytracer_python/raytracer.py
+++ raytracer_python/raytracer.py
@@ -1,46 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import camera
+from raymath import *
 
-w = 128
-h = 128
-
-def normalize(x):
-    x /= np.linalg.norm(x)
-    return x
-
-def intersect_plane(O, D, P, N):
-    # Return the distance from O to the intersection of the ray (O, D) with the 
-    # plane (P, N), or +inf if there is no intersection.
-    # O and P are 3D points, D and N (normal) are normalized vectors.
-    denom = np.dot(D, N)
-    if np.abs(denom) < 1e-6:
-        return np.inf
-
-    d = np.dot(P - O, N) / denom
-
-    if d < 0:
-        return np.inf
-
-    return d
-
-def intersect_sphere(O, D, S, R):
-    # Return the distance from O to the intersection of the ray (O, D) with the 
-    # sphere (S, R), or +inf if there is no intersection.
-    # O and S are 3D points, D (direction) is a normalized vector, R is a scalar.
-    a = np.dot(D, D)
-    OS = O - S
-    b = 2 * np.dot(D, OS)
-    c = np.dot(OS, OS) - R * R
-    disc = b * b - 4 * a * c
-    if disc > 0:
-        distSqrt = np.sqrt(disc)
-        q = (-b - distSqrt) / 2.0 if b < 0 else (-b + distSqrt) / 2.0
-        t0 = q / a
-        t1 = c / q
-        t0, t1 = min(t0, t1), max(t0, t1)
-        if t1 >= 0:
-            return t1 if t0 < 0 else t0
-    return np.inf
+w = 256
+h = 256
 
 def intersect(O, D, obj):
     if obj['type'] == 'plane':
@@ -153,6 +117,8 @@ img = np.zeros((h, w, 3))
 # Screen coordinates: x0, y0, x1, y1.
 S = (-8.0, -8.0, 8.0, 8.0)
 
+cam = camera.Camera(O, Q, 60, w, h)
+
 # Loop through all pixels.
 for i, x in enumerate(np.linspace(S[0], S[2], w)):
     if i % 10 == 0:
@@ -160,10 +126,9 @@ for i, x in enumerate(np.linspace(S[0], S[2], w)):
 
     for j, y in enumerate(np.linspace(S[1], S[3], h)):
         col[:] = 0
-        Q[:2] = (x, y)
-        D = normalize(Q - O)
         depth = 0
-        rayO, rayD = O, D
+        rayO = O
+        rayD = cam.GetWorldRay(i, h - j -1) 
         reflection = 1.
 
         # Loop through initial and secondary rays.
